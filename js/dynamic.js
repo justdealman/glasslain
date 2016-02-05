a274da5e108a2fe87a619fbc19e37ff6da02a332
Alexandr <justdealman@gmail.com>
2016-02-05 09:19:40 +0700

    original

---
+++ js/dynamic.js
@@ -0,0 +1,86 @@
+﻿function slider() {
+	$('.slider .temp > div').each(function() {
+		var path = $(this).children('img').attr('src');
+		$(this).css({
+			'background': 'url("'+path+'") no-repeat center center'
+		});
+	})
+	$('.slider .container').empty();
+	$('.slider .prev, .slider .next, .slider .pagination').remove();
+	$('.slider .container').html($('.slider .temp').html());
+	$('.slider, .slider .container, .slider .container > div').width($('.wrapper').width());
+	$('.slider').slides({
+		generatePagination: false,
+		generateNextPrev: true,
+		container: 'container',
+		effect: 'slide',
+		slideSpeed: 500,
+		slideEasing: 'easeInOutQuad',
+		play: 10000,
+		pause: 2500,
+	});
+}
+$(document).ready(function() {
+	if ( $('.slider').length > 0 ) {
+		slider();
+		$('.slider').bind('swipeleft', function() {
+			$('.slider .next').trigger('click');
+		});
+		$('.slider').bind('swiperight', function() {
+			$('.slider .prev').trigger('click');
+		});
+	}
+	if ( $('.card').length > 0 ) {
+		$('.card .photos').slides({
+			generatePagination: true,
+			generateNextPrev: true,
+			container: 'container',
+			effect: 'slide',
+			slideSpeed: 500,
+			slideEasing: 'easeInOutQuad',
+			play: 10000,
+			pause: 2500,
+		});
+		$('.card .photos').bind('swipeleft', function() {
+			$('.card .photos .next').trigger('click');
+		});
+		$('.card .photos').bind('swiperight', function() {
+			$('.card .photos .prev').trigger('click');
+		});
+	}
+	if ( $('.similar').length > 0 ) {
+		$('.similar ul').jcarousel({
+			scroll: 5,
+			animation: 500,
+			easing: 'easeInOutQuad',
+			wrap: 'circular'
+		});
+		$('.similar .jcarousel-container').bind('swipeleft', function() {
+			$('.similar .jcarousel-container .jcarousel-next').trigger('click');
+		});
+		$('.similar .jcarousel-container').bind('swiperight', function() {
+			$('.similar .jcarousel-container .jcarousel-prev').trigger('click');
+		});
+	}
+	$('input, textarea').each(function() {
+		$(this).data('holder', $(this).attr('placeholder'));
+		$(this).focusin(function() {
+			$(this).attr('placeholder', '');
+		});
+		$(this).focusout(function() {
+			$(this).attr('placeholder', $(this).data('holder'));
+		});
+	});
+});
+$(window).load(function() {
+	$('nav ul li a span').each(function() {
+		if ( $(this).height() > 20 ) {
+			$(this).css({
+				'margin-top': -$(this).outerHeight()/2+'px'
+			});
+		}
+	});
+	$('nav ul').css({
+		'opacity': '1'
+	});
+});
